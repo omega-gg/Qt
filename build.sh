76f8bf2763b8980e48f9d9bccd4f53fea62df2a7
bunjee <bunjee@omega.gg>
2020-01-22 14:52:25 +0100

    build: Add win32 (14)

--- build.sh
+++ build.sh
@@ -40,9 +40,15 @@ fi
 
 if [ $windows = true ]; then
 
+    echo "DOWNLOAD DirectX"
+
+    echo https://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe
+
     curl -L -o directX.exe https://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe
 
-    ./directX /silent
+    ls -la .
+
+    directX /silent
 
 elif [ $1 = "linux" ]; then
 
